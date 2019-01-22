6e3abf67ef24e9acb8c439ec50d2945e871a59b1
Gremious <gremious@protonmail.com>
2019-01-22 15:25:33 +0000

    orb comments!

--- theDefault/src/main/java/defaultmod/orbs/DefaultOrb.java
+++ theDefault/src/main/java/defaultmod/orbs/DefaultOrb.java
@@ -20,10 +20,13 @@ import com.megacrit.cardcrawl.vfx.combat.*;
 import defaultmod.DefaultMod;
 
 public class DefaultOrb extends AbstractOrb {
+
+    // Standard ID/Descritpion
     public static final String ORB_ID = DefaultMod.makeID("DefaultOrb");
     private static final OrbStrings orbString = CardCrawlGame.languagePack.getOrbString(ORB_ID);
     public static final String[] DESC = orbString.DESCRIPTION;
 
+    // Animation Rendering Numbers - You can leave these at default, or play around with them and see what they change.
     private float vfxTimer = 1.0f;
     private float vfxIntervalMin = 0.1f;
     private float vfxIntervalMax = 0.4f;
@@ -36,20 +39,19 @@ public class DefaultOrb extends AbstractOrb {
         this.name = orbString.NAME;
         this.img = ImageMaster.loadImage(DefaultMod.makePath(DefaultMod.DEFAULT_ORB));
 
-
         this.evokeAmount = this.baseEvokeAmount = 1;
         this.passiveAmount = this.basePassiveAmount = 3;
 
         this.updateDescription();
 
-        this.angle = MathUtils.random(360.0f);
+        this.angle = MathUtils.random(360.0f); // More Animation-related Numbers
         this.channelAnimTimer = 0.5f;
     }
 
     @Override
-    public void updateDescription() {
-        this.applyFocus();
-        this.description = DESC[0] + this.evokeAmount + DESC[1] + this.passiveAmount + DESC[2];
+    public void updateDescription() { // Set the on-hover description of the orb
+        this.applyFocus(); // Apply Focus (Look at the next method)
+        this.description = DESC[0] + this.evokeAmount + DESC[1] + this.passiveAmount + DESC[2]; // Set the description
     }
 
     @Override
@@ -59,33 +61,35 @@ public class DefaultOrb extends AbstractOrb {
     }
 
     @Override
-    public void onEvoke() {
+    public void onEvoke() { // 1.On Orb Evoke
 
-        AbstractDungeon.actionManager.addToBottom(
+        AbstractDungeon.actionManager.addToBottom( // 2.Damage all enemies
                 new DamageAllEnemiesAction(AbstractDungeon.player, DamageInfo.createDamageMatrix(this.evokeAmount, true, true), DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.NONE));
        // The damage matrix is how orb damage all enemies actions have to be assigned. For regular cards that do damage to everyone, check out cleave or whirlwind - they are a bit simpler.
 
 
-        AbstractDungeon.actionManager.addToBottom(new SFXAction("ORB_LIGHTNING_EVOKE"));
+        AbstractDungeon.actionManager.addToBottom(new SFXAction("TINGSHA")); // 3.And play a Jingle Sound.
         // For a list of sound effects you can use, look under com.megacrit.cardcrawl.audio.SoundMaster - you can see the list of keys you can use there. As far as previewing what they sound like, open desktop-1.0.jar with something like 7-Zip and go to audio. Reference the file names provided. (Thanks fiiiiilth)
 
     }
 
     @Override
-    public void onStartOfTurn() {
-        AbstractDungeon.actionManager.addToBottom(new VFXAction(new OrbFlareEffect(this, OrbFlareEffect.OrbFlareColor.FROST), 0.1f));
+    public void onStartOfTurn() {// 1.At the start of your turn.
+        AbstractDungeon.actionManager.addToBottom(// 2.This orb will have a flare effect
+                new VFXAction(new OrbFlareEffect(this, OrbFlareEffect.OrbFlareColor.FROST), 0.1f));
 
-        AbstractDungeon.actionManager.addToBottom(new DrawCardAction(AbstractDungeon.player, this.passiveAmount));
+        AbstractDungeon.actionManager.addToBottom(// 3. And draw you cards.
+                new DrawCardAction(AbstractDungeon.player, this.passiveAmount));
     }
 
     @Override
-    public void updateAnimation() {
+    public void updateAnimation() {// You can totally leave this as is.
         // If you want to create a whole new orb effect - take a look at Conspire's Water Obr. It includes a custom sound, too!
         super.updateAnimation();
         this.angle += Gdx.graphics.getDeltaTime() * 45.0f;
         this.vfxTimer -= Gdx.graphics.getDeltaTime();
         if (this.vfxTimer < 0.0f) {
-            AbstractDungeon.effectList.add(new DarkOrbPassiveEffect(this.cX, this.cY));
+            AbstractDungeon.effectList.add(new DarkOrbPassiveEffect(this.cX, this.cY)); // This is the purple-sparkles in the orb. You can change this to whatever fits your orb.
             this.vfxTimer = MathUtils.random(this.vfxIntervalMin, this.vfxIntervalMax);
         }
     }
@@ -105,13 +109,12 @@ public class DefaultOrb extends AbstractOrb {
 
 
     @Override
-    public void triggerEvokeAnimation() {
-        CardCrawlGame.sound.play("TINGSHA", 0.1f);
-        AbstractDungeon.effectsQueue.add(new DarkOrbActivateEffect(this.cX, this.cY));
+    public void triggerEvokeAnimation() { // The evoke animation of this orb is the dark-orb activation effect.
+               AbstractDungeon.effectsQueue.add(new DarkOrbActivateEffect(this.cX, this.cY));
     }
 
     @Override
-    public void playChannelSFX() {
+    public void playChannelSFX() { // When you channel this orb, the ATTACK_FIRE effect plays ("Fwoom").
         CardCrawlGame.sound.play("ATTACK_FIRE", 0.1f);
     }
 
--- theDefault/src/main/java/defaultmod/relics/DefaultClickableRelic.java
+++ theDefault/src/main/java/defaultmod/relics/DefaultClickableRelic.java
@@ -17,7 +17,7 @@ import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import com.megacrit.cardcrawl.vfx.CollectorCurseEffect;
 import defaultmod.DefaultMod;
 
-public class DefaultClickableRelic extends CustomRelic implements ClickableRelic {
+public class DefaultClickableRelic extends CustomRelic implements ClickableRelic { // You must implement things you want to use from StSlib
     /*
      * https://github.com/daviscook477/BaseMod/wiki/Custom-Relics
      * StSLib for Clickable Relics
@@ -30,7 +30,7 @@ public class DefaultClickableRelic extends CustomRelic implements ClickableRelic
     public static final String IMG = DefaultMod.makePath(DefaultMod.DEFAULT_CLICKABLE_RELIC);
     public static final String OUTLINE = DefaultMod.makePath(DefaultMod.DEFAULT_CLICKABLE_RELIC_OUTLINE);
 
-    private boolean usedThisTurn = false;
+    private boolean usedThisTurn = false; // You can also have a relic be only usable once per combat. Check out Hubris for more examples, including other StSlib things.
 
     public DefaultClickableRelic() {
         super(ID, new Texture(IMG), new Texture(OUTLINE), RelicTier.COMMON, LandingSound.MAGICAL);
@@ -42,38 +42,40 @@ public class DefaultClickableRelic extends CustomRelic implements ClickableRelic
 
 
     @Override
-    public void onRightClick() {
-        if (!isObtained || usedThisTurn) {
-            return;
+    public void onRightClick() {// On right click
+        if (!isObtained || usedThisTurn) {// If it has been used this turn, or the player doesn't actually have the relic (i.e. it's on display in the shop room)
+            return; // Don't do anything.
         }
-        if (AbstractDungeon.getCurrRoom() != null && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT) {
-            usedThisTurn = true;
-            this.flash();
-            stopPulse();
+        if (AbstractDungeon.getCurrRoom() != null && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT) { // Only if you're in combat
+            usedThisTurn = true; // Set relic as "Used this turn"
+            this.flash(); // Flash
+            stopPulse(); // And stop the pulsing animation (which is started in atPreBattle() below)
 
-            AbstractDungeon.actionManager.addToBottom(new TalkAction(true, DESCRIPTIONS[1], 4.0f, 2.0f));
-            AbstractDungeon.actionManager.addToBottom(new SFXAction("MONSTER_COLLECTOR_DEBUFF"));
-            AbstractDungeon.actionManager.addToBottom(new VFXAction(new CollectorCurseEffect(AbstractDungeon.getRandomMonster().hb.cX, AbstractDungeon.getRandomMonster().hb.cY), 2.0F));
+            AbstractDungeon.actionManager.addToBottom(new TalkAction(true, DESCRIPTIONS[1], 4.0f, 2.0f)); // Player speech bubble saying "YOU ARE MINE!" (See relic strings)
+            AbstractDungeon.actionManager.addToBottom(new SFXAction("MONSTER_COLLECTOR_DEBUFF")); // Sound Effect Action of The Collector Nails
+            AbstractDungeon.actionManager.addToBottom(new VFXAction( // Visual Effect Action of the nails applies on a random monster's position.
+                    new CollectorCurseEffect(AbstractDungeon.getRandomMonster().hb.cX, AbstractDungeon.getRandomMonster().hb.cY), 2.0F));
 
-            AbstractDungeon.actionManager.addToBottom(new EvokeOrbAction(1));
+            AbstractDungeon.actionManager.addToBottom(new EvokeOrbAction(1)); // Evoke your rightmost orb
         }
     }
 
     public void atTurnStart() {
-        usedThisTurn = false;
+        usedThisTurn = false;  // Resets the used this turn. You can remove this whole method to use a relic only once per combat rather than per turn.
+        beginLongPulse(); // Pulse while the player can click on it.
     }
 
     @Override
     public void atPreBattle()
     {
-        usedThisTurn = false;
-        beginLongPulse();
+        usedThisTurn = false; // Make sure usedThisTurn is set to false at the start of each combat.
+        beginLongPulse();     // Pulse while the player can click on it.
     }
 
     @Override
     public void onVictory()
     {
-        stopPulse();
+        stopPulse(); // Don't keep pulsing past the victory screen/outside of combat.
     }
 
     // Description
