98baab5a306aa14775e4714eb4c4a29c12b8fba6
Gremious <gremious@protonmail.com>
2019-02-26 22:17:19 +0000

    ops

--- theDefault/src/main/java/theDefault/DefaultMod.java
+++ theDefault/src/main/java/theDefault/DefaultMod.java
@@ -155,8 +155,8 @@ public class DefaultMod implements
         // CHANGE YOUR MOD ID HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         // CHANGE YOUR MOD ID HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         // CHANGE YOUR MOD ID HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        setModID("theDefaultDev");
-        // Now go to your resources folder in the project panel, and refactor> rename archetypeAPIResources to
+        setModID("theDefault");
+        // Now go to your resources folder in the project panel, and refactor> rename theDefaultResources to
         // yourModIDResources.
         // Also click on the localization > eng folder and press ctrl+shift+r, then select "Directory" (rather than in Project)
         // replace all instances of theDefault with yourModID.
